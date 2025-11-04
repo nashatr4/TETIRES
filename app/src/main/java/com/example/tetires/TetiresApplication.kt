f3716ac41ae06883fc2704e799e6cbe09c1abd5d
AdindaPutri1 <adindaputriromadhon@mail.ugm.ac.id>
2025-11-04 16:58:12 +0700

    chore: update application initialization

---
+++ app/src/main/java/com/example/tetires/TetiresApplication.kt
@@ -0,0 +1,15 @@
+package com.example.tetires
+
+import android.app.Application
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
+
+class TetiresApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (!Python.isStarted()) {
+            Python.start(AndroidPlatform(this))
+        }
+    }
+}
